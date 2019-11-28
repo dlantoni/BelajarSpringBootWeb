abb76aeb90180f2f25104d1e85c5f078fe881b3c
Antoni <antoni.d996@gmail.com>
2019-11-28 17:54:28 +0700

    learn 7

---
+++ src/main/java/com/mastahcode/BelajarSpringBootWeb/model/Mahasiswa.java
@@ -0,0 +1,51 @@
+package com.mastahcode.BelajarSpringBootWeb.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Mahasiswa {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "nama_mahasiswa")
+    private String name;
+
+    private String jurusan;
+
+    private String nim;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJurusan() {
+        return jurusan;
+    }
+
+    public void setJurusan(String jurusan) {
+        this.jurusan = jurusan;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+}
